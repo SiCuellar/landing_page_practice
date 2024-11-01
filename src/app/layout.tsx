d1ed6acc4635c70a7009b64c3fa8710846983fbc
SiCuellar <cuellarsilvestre@gmail.com>
2024-11-01 13:52:10 -0700

    adds header

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { DM_Sans } from "next/font/google";
 import "./globals.css";
 import clsx from "clsx";
+import { twMerge } from 'tailwind-merge';
+
 
 const dmSans = DM_Sans({ subsets: ["latin"] });
 
@@ -23,3 +25,4 @@ export default function RootLayout({
     </html>
   );
 }
+ 
